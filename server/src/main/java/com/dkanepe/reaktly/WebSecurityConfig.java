08ebf92b62c090cfe8a59349a9b29fd294e56876
Daniels Kanepe <danixkanepe@gmail.com>
2022-10-10 22:41:41 +0200

    CORS fix: make client work on any host

--- server/src/main/java/com/dkanepe/reaktly/WebSecurityConfig.java
+++ server/src/main/java/com/dkanepe/reaktly/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
