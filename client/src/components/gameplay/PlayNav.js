59821e32b73d654bc16af7ac4130e01158b87df7
Daniels Kanepe <danixkanepe@gmail.com>
2022-10-12 16:02:49 +0200

    Red timer < 3 seconds
    To create a sense of urgency :)

--- client/src/components/gameplay/PlayNav.js
+++ client/src/components/gameplay/PlayNav.js
@@ -9,10 +9,23 @@ function PlayNav(props) {
     // timer renderer
     const renderer = ({ hours, minutes, seconds, completed }) => {
         return (
-            <span>
-                {minutes.toString().padStart(2, '0')}:
-                {seconds.toString().padStart(2, '0')}
-            </span>
+            // text-bg-primary when more than 3 seconds left
+            <div className={"border rounded border-0 py-1 px-3" + (seconds > 3 ? " text-bg-primary" : " text-bg-danger")}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                     viewBox="0 0 16 16" className="bi bi-clock d-none d-sm-inline-block">
+                    <path
+                        d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"></path>
+                    <path
+                        d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"></path>
+                </svg>
+                <span className="border-0 ps-sm-2">
+                    <span>
+                        {minutes.toString().padStart(2, '0')}:
+                        {seconds.toString().padStart(2, '0')}
+
+                    </span>
+                </span>
+            </div>
         );
     };
 
@@ -38,18 +51,7 @@ function PlayNav(props) {
                         </div>
                         <div className="col d-flex justify-content-center align-items-center">
                             {props.timer &&
-                                <div className="text-bg-primary border rounded border-0 py-1 px-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                                         viewBox="0 0 16 16" className="bi bi-clock d-none d-sm-inline-block">
-                                        <path
-                                            d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"></path>
-                                        <path
-                                            d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"></path>
-                                    </svg>
-                                    <span className="border-0 ps-sm-2">
                                     <Countdown date={props.timer} renderer={renderer} key={props.timer}/>
-                                </span>
-                                </div>
                             }
 
                         </div>
